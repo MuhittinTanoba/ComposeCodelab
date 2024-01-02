204625c494d148abd880979dab5e21bea478313f
Muhittin Tanoba <muhitan2002@gmail.com>
2024-01-02 13:21:24 +0300

    Animation Codelab

--- ThemingCodelab/app/src/main/java/com/example/reply/ui/components/ReplyEmailListItem.kt
+++ ThemingCodelab/app/src/main/java/com/example/reply/ui/components/ReplyEmailListItem.kt
@@ -84,7 +84,8 @@ fun ReplyEmailListItem(
 
                     Text(
                         text = email.createdAt,
-                        style = MaterialTheme.typography.labelMedium
+                        style = MaterialTheme.typography.labelMedium,
+                        color = MaterialTheme.colorScheme.onSurfaceVariant
                     )
 
 
@@ -112,8 +113,9 @@ fun ReplyEmailListItem(
                 text = email.body,
                 maxLines = 2,
                 style = MaterialTheme.typography.bodyLarge,
-                overflow = TextOverflow.Ellipsis
-            )
+                overflow = TextOverflow.Ellipsis,
+                color = MaterialTheme.colorScheme.onSurfaceVariant,
+                )
         }
     }
 }
