52ad0022ce1b391bca4329c9671f82915fb231ca
Muhittin Tanoba <muhitan2002@gmail.com>
2024-01-06 11:50:25 +0300

    UI state production pipeline

--- AdvancedStateAndSideEffectsCodelab/app/src/main/java/androidx/compose/samples/crane/home/MainViewModel.kt
+++ AdvancedStateAndSideEffectsCodelab/app/src/main/java/androidx/compose/samples/crane/home/MainViewModel.kt
@@ -23,18 +23,27 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 import kotlin.random.Random
 
 const val MAX_PEOPLE = 4
-
 @HiltViewModel
 class MainViewModel @Inject constructor(
     private val destinationsRepository: DestinationsRepository,
     @DefaultDispatcher private val defaultDispatcher: CoroutineDispatcher
+
 ) : ViewModel() {
+
+    private val _suggestedDestinations = MutableStateFlow<List<ExploreModel>>(emptyList())
+    val suggestedDestinations: StateFlow<List<ExploreModel>> = _suggestedDestinations.asStateFlow()
+    init {
+        _suggestedDestinations.value = destinationsRepository.destinations
+    }
 
     val hotels: List<ExploreModel> = destinationsRepository.hotels
     val restaurants: List<ExploreModel> = destinationsRepository.restaurants
@@ -42,15 +51,13 @@ class MainViewModel @Inject constructor(
     fun updatePeople(people: Int) {
         viewModelScope.launch {
             if (people > MAX_PEOPLE) {
-            // TODO Codelab: Uncomment
-            //  _suggestedDestinations.value = emptyList()
+            _suggestedDestinations.value = emptyList()
             } else {
                 val newDestinations = withContext(defaultDispatcher) {
                     destinationsRepository.destinations
                         .shuffled(Random(people * (1..100).shuffled().first()))
                 }
-                // TODO Codelab: Uncomment
-                //  _suggestedDestinations.value = newDestinations
+                 _suggestedDestinations.value = newDestinations
             }
         }
     }
@@ -61,8 +68,7 @@ class MainViewModel @Inject constructor(
                 destinationsRepository.destinations
                     .filter { it.city.nameToDisplay.contains(newDestination) }
             }
-            // TODO Codelab: Uncomment
-            //  _suggestedDestinations.value = newDestinations
+            _suggestedDestinations.value = newDestinations
         }
     }
 }
